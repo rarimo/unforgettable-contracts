f7f101a6c3be57a0480aefb924eb7c1599175d46
Oleh Komendant <oleg02kom@gmail.com>
2025-07-29 13:51:05 +0300

    Update Vaults deployment scripts

--- deploy/config/localhost.ts
+++ deploy/config/localhost.ts
@@ -6,11 +6,13 @@ export const deployConfig: DeployConfig = {
   contractsOwner: "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266",
   vaultsConfig: {
     basePeriodDuration: 3600n * 24n * 30n,
+    vaultNameRetentionPeriod: 3600n * 24n * 30n * 12n,
     subscriptionSigner: "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266",
     paymentTokenConfigs: [
       {
         paymentToken: ETHER_ADDR,
         baseSubscriptionCost: wei(1, 16),
+        baseVaultNameCost: wei(5, 15),
       },
     ],
     sbtTokenConfigs: [],
--- deploy/vaults/2_VaultSubscriptionManager.migration.ts
+++ deploy/vaults/2_VaultSubscriptionManager.migration.ts
@@ -8,23 +8,17 @@ export = async (deployer: Deployer) => {
   const config = await getConfig();
 
   const vaultSubscriptionManagerInitData = VaultSubscriptionManager__factory.createInterface().encodeFunctionData(
-    "initialize(uint64,address,(address,uint256)[],(address,uint64)[])",
+    "initialize(uint64,uint64,address,(address,uint256,uint256)[],(address,uint64)[])",
     [
       config.vaultsConfig.basePeriodDuration,
+      config.vaultsConfig.vaultNameRetentionPeriod,
       config.vaultsConfig.subscriptionSigner,
       config.vaultsConfig.paymentTokenConfigs,
       config.vaultsConfig.sbtTokenConfigs,
     ],
   );
 
-  await deployer.deployProxy(
-    VaultSubscriptionManager__factory,
-    "ERC1967Proxy",
-    (implementationAddress: string): any[] => {
-      return [implementationAddress, vaultSubscriptionManagerInitData];
-    },
-    {
-      name: "VaultSubscriptionManager",
-    },
-  );
+  await deployer.deployERC1967Proxy(VaultSubscriptionManager__factory, vaultSubscriptionManagerInitData, {
+    name: "VaultSubscriptionManager",
+  });
 };

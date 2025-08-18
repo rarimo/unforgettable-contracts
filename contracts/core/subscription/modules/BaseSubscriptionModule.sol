4285e7e2f533d951a730c72d5904c1322aceb29c
Oleh Komendant <oleg02kom@gmail.com>
2025-08-18 13:29:46 +0300

    Refactor subscription modules

---
+++ contracts/core/subscription/modules/BaseSubscriptionModule.sol
@@ -0,0 +1,80 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
+
+import {IBaseSubscriptionModule} from "../../../interfaces/core/IBaseSubscriptionModule.sol";
+
+contract BaseSubscriptionModule is IBaseSubscriptionModule {
+    bytes32 private constant BASE_SUBSCRIPTION_MODULE_STORAGE_SLOT =
+        keccak256("unforgettable.contract.base.subscription.module.storage");
+
+    struct BaseSubscriptionModuleStorage {
+        mapping(address => AccountSubscriptionData) accountsSubscriptionData;
+    }
+
+    function _getBaseSubscriptionModuleStorage()
+        private
+        pure
+        returns (BaseSubscriptionModuleStorage storage _bsms)
+    {
+        bytes32 slot_ = BASE_SUBSCRIPTION_MODULE_STORAGE_SLOT;
+
+        assembly ("memory-safe") {
+            _bsms.slot := slot_
+        }
+    }
+
+    function hasSubscription(address account_) public view virtual returns (bool) {
+        return getSubscriptionStartTime(account_) > 0;
+    }
+
+    function hasActiveSubscription(address account_) public view virtual returns (bool) {
+        return block.timestamp < getSubscriptionEndTime(account_);
+    }
+
+    function hasSubscriptionDebt(address account_) public view virtual returns (bool) {
+        return !hasActiveSubscription(account_) && hasSubscription(account_);
+    }
+
+    function getSubscriptionStartTime(address account_) public view virtual returns (uint64) {
+        return _getAccountSubscriptionData(account_).startTime;
+    }
+
+    function getSubscriptionEndTime(address account_) public view virtual returns (uint64) {
+        if (!hasSubscription(account_)) {
+            return uint64(block.timestamp);
+        }
+
+        return _getAccountSubscriptionData(account_).endTime;
+    }
+
+    function _extendSubscription(address account_, uint64 duration_) internal virtual {
+        uint64 subscriptionEndTime_ = getSubscriptionEndTime(account_);
+        uint64 newEndTime_ = subscriptionEndTime_ + duration_;
+
+        if (!hasSubscription(account_)) {
+            _setStartTime(account_, uint64(block.timestamp));
+        }
+
+        _setEndTime(account_, newEndTime_);
+
+        emit SubscriptionExtended(account_, duration_, newEndTime_);
+    }
+
+    function _setStartTime(address account_, uint64 newStartTime_) internal virtual {
+        _getAccountSubscriptionData(account_).startTime = newStartTime_;
+    }
+
+    function _setEndTime(address account_, uint64 newEndTime_) internal virtual {
+        _getAccountSubscriptionData(account_).endTime = newEndTime_;
+    }
+
+    function _checkAddress(address addr_, string memory fieldName_) internal pure {
+        require(addr_ != address(0), ZeroAddr(fieldName_));
+    }
+
+    function _getAccountSubscriptionData(
+        address account_
+    ) private view returns (AccountSubscriptionData storage) {
+        return _getBaseSubscriptionModuleStorage().accountsSubscriptionData[account_];
+    }
+}
---
+++ contracts/interfaces/core/IBaseSubscriptionModule.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
+
+interface IBaseSubscriptionModule {
+    struct AccountSubscriptionData {
+        uint64 startTime;
+        uint64 endTime;
+    }
+
+    error ZeroAddr(string fieldName);
+
+    event SubscriptionExtended(address indexed account, uint64 duration, uint64 newEndTime);
+
+    function hasSubscription(address account_) external view returns (bool);
+    function hasActiveSubscription(address account_) external view returns (bool);
+    function hasSubscriptionDebt(address account_) external view returns (bool);
+    function getSubscriptionEndTime(address account_) external view returns (uint64);
+    function getSubscriptionStartTime(address account_) external view returns (uint64);
+}
